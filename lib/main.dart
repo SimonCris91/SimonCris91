40bd8a24b5a7ac9930a041606a282eee050908cd
Simone Cris Feo <simoncris91@gmail.com>
2025-11-10 19:12:29 +0100

    Main.dart completo per test moduli avanzati

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,24 @@
-import 'package:flutter/material.dart';
 import 'modules/maya/maya.dart';
 import 'modules/human_design/human_design.dart';
 import 'modules/astrology/astrology.dart';
 
 void main() {
-  // Esempio: inizializza i moduli
-  MayaModule().run();
-  HumanDesignModule().run();
-  AstrologyModule().run();
+  print('--- MegaSegni AI Test ---\n');
 
-  runApp(MegaSegniApp());
-}
+  // Test Modulo Maya
+MayaModule maya = MayaModule();
+String kin = maya.getKin(8, 2); // esempio: 8 Febbraio
+print('Giorno Kin per 8/2: $kin');
 
-class MegaSegniApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MegaSegni AI',
-      home: Scaffold(
-        appBar: AppBar(title: Text('MegaSegni AI')),
-        body: Center(child: Text('Benvenuto su MegaSegni AI!')),
-      ),
-    );
-  }
+  // Test Modulo Human Design
+  HumanDesignModule human = HumanDesignModule();
+  String humanProfile = human.getProfile(1991);
+  print('Profilo Human Design per 1991: $humanProfile');
+
+  // Test Modulo Astrologia
+  AstrologyModule astrology = AstrologyModule();
+  String zodiacSign = astrology.getWesternSign(8, 2);
+  print('Segno Zodiacale per 8/2: $zodiacSign');
+
+  print('\n--- Test completato ---');
 }
